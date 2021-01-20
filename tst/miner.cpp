9ccbd8ca886e0f9579552143eebd6d1b79fedcb8
William Morriss <wjmelements@gmail.com>
2021-01-19 18:34:44 -0800

    test block parent

--- tst/miner.cpp
+++ tst/miner.cpp
@@ -5,8 +5,12 @@
 
 void test_miner_index() {
     initMiners(3);
-    block_t *block = mineBlock(&miners[1], NULL);
-    assert(block->miner == 1);
+    block_t *block0 = mineBlock(&miners[1], NULL);
+    assert(block0->miner == 1);
+    assert(block0->parent == NULL);
+    block_t *block1 = mineBlock(&miners[2], block0);
+    assert(block1->parent == block0);
+    assert(block1->miner == 2);
 }
 
 int main() {
